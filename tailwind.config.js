9e0b94a9fd4b78462608f922ec499c83a274fc16
whale22 <realwhale64@naver.com>
2025-02-11 17:06:25 +0900

    feat: 슬라이더 기능 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}", // 프로젝트 내에서 Tailwind가 적용될 파일 경로
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("daisyui"), // daisyUI 플러그인 추가
-  ],
+  plugins: [require("daisyui")],
   daisyui: {
-      themes: [],
+    themes: ["light"], // 기본 테마를 "light"로 설정
   },
 };
