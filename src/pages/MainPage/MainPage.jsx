a70642e1ec6a4663263b9b262da223b7571fdec7
daehyuk1231 <daehyuk1231@naver.com>
2025-02-11 10:47:05 +0900

    fix : 상세 페이지 이동 수정

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,8 +2,11 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './MainPage.css';
 import { Link } from 'react-router-dom';
+import { API_BACKEND_URL } from "../../config";
 
 import Banner from '../../components/banners/Banner';
+// 모든 axios 요청에 쿠키 포함
+// axios.defaults.withCredentials = true;
 
 function MovieSection({ title, movies, loading, error }) {
     return (
@@ -38,14 +41,55 @@ function MovieSection({ title, movies, loading, error }) {
 function MainPage() {
     const [popularMovies, setBoxOfficeMovies] = useState([]);
     const [topRatedMovies, setRecommendedMovies] = useState([]);
-    const [actionMovies, setActionMovies] = useState([]);
-    const [comedyMovies, setComedyMovies] = useState([]);
-    const [animationMovies, setAnimationMovies] = useState([]);
+
+    const [likeKeywordMovies, setLikeKeywordMovies] = useState([]);
+    const [likeGenreMovies, setLikeGenreMovies] = useState([]);
+    const [likeGenreMovies_2nd, setLikeGenreMovies_2nd] = useState([]);
+
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    // ⭐ 새로고침 시 쿠키를 가져와 상태 유지
+    useEffect(() => {
+        const checkCookie = async () => {
+            try {
+                const res = await axios.get(`${API_BACKEND_URL}/api/v1/auth/check`, {
+                    withCredentials: true  // 쿠키 포함
+                });
+                console.log('로그인 성공, 쿠키 확인:', res.data);
+            } catch (err) {
+                console.log('비로그인 상태');
+                //                 console.error('쿠키 확인 실패:', err);
+            }
+        };
+        checkCookie();
+    }, []);
+
     // 로컬 데이터 설정
     const localPopularMovies = [
+        { id: 1 },
+        { id: 2 },
+        { id: 3 },
+        { id: 4 },
+        { id: 5 },
+        { id: 6 },
+        { id: 7 },
+        { id: 8 },
+        { id: 9 },
+    ];
+
+    const localTopRatedMovies = [
+        { id: 1 },
+        { id: 2 },
+        { id: 3 },
+        { id: 4 },
+        { id: 5 },
+        { id: 6 },
+        { id: 7 },
+        { id: 8 },
+        { id: 9 },
+    ];
+    const localLikeGenreMovies = [
         { id: 1 },
         { id: 2 },
         { id: 3 },
@@ -61,28 +105,42 @@ function MainPage() {
         const fetchMovies = async () => {
             try {
                 setLoading(true);
+
                 // 백엔드 상태 확인: 헬스 체크로 서버가 동작하는지 확인
                 const backendAvailable = await checkBackendAvailability();
 
                 if (backendAvailable) {
-                    const popularMovies = await axios.get('http://localhost:8090/api/v1/movie/popular');
+                    const popularMovies = await axios.get(`${API_BACKEND_URL}/api/v1/movie/popular`);
                     setBoxOfficeMovies(popularMovies.data);
 
-                    const topRatedMovies = await axios.get('http://localhost:8090/api/v1/movie/top-rated');
+                    const topRatedMovies = await axios.get(`${API_BACKEND_URL}/api/v1/movie/top-rated`);
                     setRecommendedMovies(topRatedMovies.data);
 
-                    const actionMovies = await axios.get('http://localhost:8090/api/v1/movie/genre/action');
-                    setActionMovies(actionMovies.data);
+                    // 좋아하는 키워드의 영화
+                    const likeKeywordMovies = await axios.get(`${API_BACKEND_URL}/api/v1/movie/likes_keyword`, {
+                        withCredentials: true
+                    });
+                    setLikeKeywordMovies(likeKeywordMovies.data);
 
-                    const comedyMovies = await axios.get('http://localhost:8090/api/v1/movie/genre/comedy');
-                    setComedyMovies(comedyMovies.data);
 
-                    const animationMovies = await axios.get('http://localhost:8090/api/v1/movie/genre/animation');
-                    setAnimationMovies(animationMovies.data);
+                    // 좋아하는 첫 번째 장르의 영화
+                    const likeGenreMovies = await axios.get(`${API_BACKEND_URL}/api/v1/movie/likes`, {
+                        withCredentials: true
+                    });
+                    setLikeGenreMovies(likeGenreMovies.data);
+
+                    // 좋아하는 두 번째 장르의 영화
+                    const likeGenreMovies_2nd = await axios.get(`${API_BACKEND_URL}/api/v1/movie/likes_2nd`, {
+                        withCredentials: true
+                    });
+                    setLikeGenreMovies_2nd(likeGenreMovies_2nd.data);
                 } else {
                     // 백엔드 꺼져 있을 때 로컬 데이터를 사용
                     setBoxOfficeMovies(localPopularMovies);
-                    setRecommendedMovies(localPopularMovies);
+                    setRecommendedMovies(localTopRatedMovies);
+                    setLikeKeywordMovies(localLikeGenreMovies);
+                    setLikeGenreMovies(localLikeGenreMovies);
+                    setLikeGenreMovies_2nd(localLikeGenreMovies);
                 }
             } catch (error) {
                 console.error('영화 데이터를 불러오는 데 실패했습니다.', error);
@@ -95,7 +153,7 @@ function MainPage() {
         // 백엔드 서버가 동작하는지 체크하는 함수
         const checkBackendAvailability = async () => {
             try {
-                await axios.get('http://localhost:8090/api/v1/movie/top-rated');
+                await axios.get(`${API_BACKEND_URL}/api/v1/movie/top-rated`);
                 return true; // 서버가 정상
             } catch (err) {
                 return false; // 서버가 비정상
@@ -114,14 +172,86 @@ function MainPage() {
                 {/* 추천 영화 섹션 */}
                 <MovieSection title="TopRatedMovies" movies={topRatedMovies} loading={loading} error={error} />
 
-                {/* 액션 영화 섹션 */}
-                <MovieSection title="ActionMovies" movies={actionMovies} loading={loading} error={error} />
+                {/* 좋아하는 키워드 영화 섹션 */}
+                <div className="box-recommend">
+                    <p className="contents-title">당신의 취향을 찾아드릴게요!</p>
+                    <div className="contents-box">
+                        {loading ? (
+                            <p>로딩 중...</p>
+                        ) : error ? (
+                            <p>{error}</p>  // 에러 메시지 표시
+                        ) : (
+                            <ul className="contents-ul">
+                                {likeKeywordMovies.map((movie) => (
+                                    <li key={movie.id}>
+                                        <Link to={`/detail/${movie.id}`}>
+                                            <img
+                                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                                alt={movie.title}
+                                                className="movie-poster"
+                                            />
+                                            {movie.title}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+                </div>
 
-                {/* 코미디 영화 섹션 */}
-                <MovieSection title="ComedyMovies" movies={comedyMovies} loading={loading} error={error} />
+                {/* 좋아하는 장르 영화 섹션 */}
+                <div className="box-recommend">
+                    <p className="contents-title">당신이 가장 좋아하는 장르 영화</p>
+                    <div className="contents-box">
+                        {loading ? (
+                            <p>로딩 중...</p>
+                        ) : error ? (
+                            <p>{error}</p>  // 에러 메시지 표시
+                        ) : (
+                            <ul className="contents-ul">
+                                {likeGenreMovies.map((movie) => (
+                                    <li key={movie.id}>
+                                        <Link to={`/detail/${movie.id}`}>
+                                            <img
+                                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                                alt={movie.title}
+                                                className="movie-poster"
+                                            />
+                                            {movie.title}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+                </div>
 
-                {/* 애니메이션 영화 섹션 */}
-                <MovieSection title="AnimationMovies" movies={animationMovies} loading={loading} error={error} />
+                {/* 두 번째로 좋아하는 장르 영화 섹션 */}
+                <div className="box-recommend">
+                    <p className="contents-title">당신이 정말 좋아하는 장르 영화</p>
+                    <div className="contents-box">
+                        {loading ? (
+                            <p>로딩 중...</p>
+                        ) : error ? (
+                            <p>{error}</p>  // 에러 메시지 표시
+                        ) : (
+                            <ul className="contents-ul">
+                                {likeGenreMovies_2nd.map((movie) => (
+                                    <li key={movie.id}>
+                                        <Link to={`/detail/${movie.id}`}>
+                                            <img
+                                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                                alt={movie.title}
+                                                className="movie-poster"
+                                            />
+                                            {movie.title}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
     );
